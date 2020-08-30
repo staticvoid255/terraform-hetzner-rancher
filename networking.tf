160f8c08a95d9efd06bc3b91c513e5a8f03d35c3
Static Void <staticvoidnetwork@outlook.com>
2020-08-30 21:03:56 +0200

    add resource dependency for dns host creation, comment out assignment of ip to servers on the top-level network

--- networking.tf
+++ networking.tf
@@ -3,6 +3,8 @@ data "hetznerdns_zone" "dns_zone" {
 }
 
 resource "hetznerdns_record" "web" {
+    depends_on = [hcloud_server.rancher_host]
+    
     zone_id = data.hetznerdns_zone.dns_zone.id
     name = var.rancher_host_name
     value = hcloud_server.rancher_host.ipv4_address
@@ -15,10 +17,12 @@ resource "hcloud_network" "rancher_primary_network" {
     ip_range          = "10.0.1.0/8"
 }
 
-resource "hcloud_server_network" "rancher_host_network_assignment" {
-  server_id = hcloud_server.rancher_host.id
-  network_id = hcloud_network.rancher_primary_network.id
-}
+/*resource "hcloud_server_network" "rancher_host_network_assignment" {
+    depends_on = [hcloud_network.rancher_primary_network]
+
+    server_id = hcloud_server.rancher_host.id
+    network_id = hcloud_network.rancher_primary_network.id
+}*/
 
 resource "hcloud_network_subnet" "rancher_cluster_subnet" {
     depends_on = [hcloud_network.rancher_primary_network]
@@ -30,6 +34,6 @@ resource "hcloud_network_subnet" "rancher_cluster_subnet" {
 }
 
 resource "hcloud_server_network" "rancher_host_subnet_assignment" {
-  server_id = "${hcloud_server.rancher_host.id}"
-  network_id = "${hcloud_network.rancher_cluster_subnet.id}"
+    server_id = "${hcloud_server.rancher_host.id}"
+    subnet_id = "${hcloud_network_subnet.rancher_cluster_subnet.id}"
 }
